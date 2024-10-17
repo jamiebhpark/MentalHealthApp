195e6700dc50d1528cd4a705e1612d6d40d746ee
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-10-17 16:07:58 +0900

    Initial Commit

---
+++ MentalHealthApp/MentalHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthAppApp.swift
+//  MentalHealthApp
+//
+//  Created by JONGHUN PARK on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MentalHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
